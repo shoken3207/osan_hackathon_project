2f1d91452d143e1fc72abe894657c59767626b4b
shoken <shoken4797@gmail.com>
2024-08-10 18:41:39 +0900

    typeエラー　解消

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageUpload = () => {
   const handleChange = (e: React.InputHTMLAttributes<HTMLInputElement>) => {
-    console.log(e.target.files[0]);
+    // console.log(e.target.files[0]);
   };
   return (
     <div>
